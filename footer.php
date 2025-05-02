71cd2ff03d016d29ebc9a31105275c8e32eda0b6
dare-tunmise <daretunmise@yahoo.com>
2025-05-02 10:30:37 +0100

    ...

--- footer.php
+++ footer.php
@@ -19,5 +19,6 @@
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 <script src="./app.js"></script>
+<?php wp_footer(); ?>
 </body>
 </html>
---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php wp_head(); ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles.css">
+    <title>Writer's Portfolio</title>
+</head>
+<body>
+    <header>
+        <div class="logo">RASAQ MALIK G.</div>
+        <nav id="nav">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Books</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Publications</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+        <div class="hamburger" id="hamburger">
+            <span class="line"></span>
+            <span class="line"></span>
+        </div>
+    </header>
