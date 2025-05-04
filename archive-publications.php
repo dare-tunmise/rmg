58da21485202952d8c474a6a3e0f6b576c16ce8a
dare-tunmise <daretunmise@yahoo.com>
2025-05-04 16:38:16 +0100

    ...

--- archive-publications.php
+++ archive-publications.php
@@ -1,127 +1,119 @@
 <?php 
 get_header(); 
 ?>
-    <section class="first-container" style="max-width: 800px; margin: 5rem auto; padding:1rem  2rem;">
-        <h1 style="margin: 1.5rem 0; color: #000000; font-weight: 700; font-family: condensed; font-size:2.5rem;">Publications</h1>
 
-        <div class="pub-container">
-            <h3 style="margin: 2rem 0; color: #000000;">SELECTED POEMS</h3>
-            <?php 
-            while(have_posts()) {
-                the_post();  
-                $genre = get_field('genre');
-                if ($genre == 'poetry') : ?>
-                    <div class="pub" style="margin:1rem 0;">
-                        <h4 style="color: #000000; font-family: condensed; font-weight: 400; font-size:14px; text-transform: uppercase; line-height: 1em; letter-spacing: .12em;">
-                            <?php echo get_field('journal') ?>
-                        </h4>
-                        <p style="text-transform: Capitalize; font-family: garamond; margin:1rem 0 0 1rem;">
-                            <a href="<?php echo get_field('link') ?>" style="color: #000000; text-transform: capitalize;">
-                                “<?php echo get_field('title'); ?>”
-                            </a>
-                        </p>
-                    </div>
-                <?php endif; 
-            } 
-            ?>
+<section class="publications-container">
+    <h1 class="publications-title">Publications</h1>
+
+    <div class="publications-section">
+        <h3 class="section-heading">Selected Poems</h3>
+        <?php 
+        while(have_posts()) {
+            the_post();  
+            $genre = get_field('genre');
+            if ($genre == 'poetry') : ?>
+                <div class="publication-item">
+                    <h4 class="publication-journal"><?php echo get_field('journal') ?></h4>
+                    <p class="publication-title">
+                        <a href="<?php echo get_field('link') ?>">
+                            “<?php echo get_field('title'); ?>”
+                        </a>
+                    </p>
+                </div>
+            <?php endif; 
+        } 
+        wp_reset_postdata();
+        ?>
+    </div>
+
+    <div class="publications-section">
+        <h3 class="section-heading">Selected Essays</h3>
+        <?php 
+        rewind_posts();
+        while(have_posts()) {
+            the_post();  
+            $genre = get_field('genre');
+            if ($genre == 'essay') : ?>
+                <div class="publication-item">
+                    <h4 class="publication-journal"><?php echo get_field('journal') ?></h4>
+                    <p class="publication-title">
+                        <a href="<?php echo get_field('link') ?>">
+                            “<?php echo get_field('title'); ?>”
+                        </a>
+                    </p>
+                </div>
+            <?php endif; 
+        } 
+        wp_reset_postdata();
+        ?>
+    </div>
+
+    <div class="publications-section">
+        <h3 class="section-heading">Selected Interviews</h3>
+        <ul class="publication-list">
+        <?php 
+        rewind_posts();
+        while(have_posts()) {
+            the_post();  
+            $genre = get_field('genre');
+            if ($genre == 'interview') : ?>
+                <li class="publication-list-item">
+                    <a href="<?php echo get_field('link') ?>">
+                        <?php echo get_field('journal') ?>
+                    </a>
+                </li>
+            <?php endif; 
+        } 
+        wp_reset_postdata();
+        ?>
+        </ul>
+    </div>
+
+    <div class="publications-section">
+        <h3 class="section-heading">Selected Reviews</h3>
+        <ul class="publication-list">
+        <?php 
+        rewind_posts();
+        while(have_posts()) {
+            the_post();  
+            $genre = get_field('genre');
+            if ($genre == 'review') : ?>
+                <li class="publication-list-item">
+                    <a href="<?php echo get_field('link') ?>">
+                        <?php echo get_field('journal') ?>
+                    </a>
+                </li>
+            <?php endif; 
+        } 
+        wp_reset_postdata();
+        ?>
+        </ul>
+    </div>
+
+    <div class="publications-section">
+        <h3 class="section-heading">Selected Nonfiction</h3>
+        <?php 
+        rewind_posts();
+        while(have_posts()) {
+            the_post();  
+            $genre = get_field('genre');
+            if ($genre == 'nonfiction') : ?>
+                <div class="publication-item">
+                    <h4 class="publication-journal"><?php echo get_field('journal') ?></h4>
+                    <p class="publication-title">
+                        <a href="<?php echo get_field('link') ?>">
+                            “<?php echo get_field('title'); ?>”
+                        </a>
+                    </p>
+                </div>
+            <?php endif;
+        } 
+        wp_reset_postdata();
+        ?>
+        <div class="publications-pagination">
+            <?php echo paginate_links(); ?>
         </div>
-
-        <div class="pub-container">
-            <h3 style="margin: 2rem 0; color: #000000;">SELECTED ESSAYS</h3>
-            <?php 
-            while(have_posts()) {
-                the_post();  
-                $genre = get_field('genre');
-                if ($genre == 'essay') : ?>
-                    <div class="pub" style="margin:1rem 0;">
-                        <h4 style="color: #000000; font-family: condensed; font-weight: 400; font-size:14px; text-transform: uppercase; line-height: 1em; letter-spacing: .12em;">
-                            <?php echo get_field('journal') ?>
-                        </h4>
-                        <p style="text-transform: Capitalize; font-family: garamond; margin:1rem 0 0 1rem;">
-                            <a href="<?php echo get_field('link') ?>" style="color: #000000; text-transform: capitalize;">
-                                “<?php echo get_field('title'); ?>”
-                            </a>
-                        </p>
-                    </div>
-                <?php endif; 
-            } 
-            ?>
-        </div>
-
-        <div class="pub-container">
-            <h3 style="margin: 1rem 0; color: #000000;">SELECTED INTERVIEWS</h3>
-            <ul style="padding-left: 20px;">
-            <?php 
-            while(have_posts()) {
-                the_post();  
-                $genre = get_field('genre');
-                if ($genre == 'interview') : ?>
-                     <li><a href="<?php echo get_field('link') ?>" style="color: #000000; font-weight: 500; font-size:20px;"> <?php echo get_field('journal') ?></a></li>
-                    <!-- <div class="pub" style="margin:1rem 0;">
-
-                        <h4 style="color: #000000; font-family: condensed; font-weight: 400; font-size:14px; text-transform: uppercase; line-height: 1em; letter-spacing: .12em;">
-                            <?php echo get_field('journal') ?>
-                        </h4>
-                        <p style="text-transform: Capitalize; font-family: garamond; margin:1rem 0 0 1rem;">
-                            <a href="<?php echo get_field('link') ?>" style="color: #000000; text-transform: capitalize;">
-                                “<?php echo get_field('title'); ?>”
-                            </a>
-                        </p>
-                    </div> -->
-                <?php endif; 
-            } 
-            ?>
-            </ul>
-        </div>
-
-        <div class="pub-container">
-            <h3 style="margin: 2rem 0; color: #000000;">SELECTED REVIEWS</h3>
-            <ul>
-            <?php 
-            while(have_posts()) {
-                the_post();  
-                $genre = get_field('genre');
-                if ($genre == 'review') : ?>
-                 <li><a href="<?php echo get_field('link') ?>" style="color: #000000; font-family: condensed; font-weight: 500; font-size:25px;"> <?php echo get_field('journal') ?></a></li>
-                    <!-- <div class="pub" style="margin:1rem 0;">
-                        <h4 style="color: #000000; font-family: condensed; font-weight: 400; font-size:14px; text-transform: uppercase; line-height: 1em; letter-spacing: .12em;">
-                            <?php echo get_field('journal') ?>
-                        </h4>
-                        <p style="text-transform: Capitalize; font-family: garamond; margin:1rem 0 0 1rem;">
-                            <a href="<?php echo get_field('link') ?>" style="color: #000000; text-transform: capitalize;">
-                                “<?php echo get_field('title'); ?>”
-                            </a>
-                        </p>
-                    </div> -->
-                <?php endif; 
-            } 
-            ?>
-            </ul>
-        </div>
-
-        <div class="pub-container">
-            <h3 style="margin: 2rem 0; color: #000000;">SELECTED NONFICTION</h3>
-            <?php 
-            while(have_posts()) {
-                the_post();  
-                $genre = get_field('genre');
-                if ($genre == 'nonfiction') : ?>
-                    <div class="pub" style="margin:1rem 0;">
-                        <h4 style="color: #000000; font-family: condensed; font-weight: 400; font-size:14px; text-transform: uppercase; line-height: 1em; letter-spacing: .12em;">
-                            <?php echo get_field('journal') ?>
-                        </h4>
-                        <p style="text-transform: Capitalize; font-family: garamond; margin:1rem 0 0 1rem;">
-                            <a href="<?php echo get_field('link') ?>" style="color: #000000; text-transform: capitalize;">
-                                “<?php echo get_field('title'); ?>”
-                            </a>
-                        </p>
-                    </div>
-                <?php endif;
-            } 
-
-            echo paginate_links();
-            ?>
-        </div>
-    </section>
+    </div>
+</section>
 
 <?php get_footer(); ?>
